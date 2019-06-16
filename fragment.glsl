53190c19278e02a0fc4199e97f95d523e5a282dc
Stefan Mijucic <fanna993@gmail.com>
2019-06-16 13:22:15 +0200

    Add smoothing

--- fragment.glsl
+++ fragment.glsl
@@ -10,6 +10,11 @@ uniform float time;
 
 const float EPSILON = .0001;
 
+float smin( float a, float b, float k ) {
+    float h = clamp( 0.5+0.5*(b-a)/k, 0., 1. );
+    return mix( b, a, h ) - k*h*(1.0-h);
+}
+
 float planeSDF(vec3 p){
     return p.y;
 }
@@ -41,9 +46,9 @@ float sceneSDF(vec3 p){
     float cubeDist=cubeSDF(p,c);
     float planeDist=planeSDF(p);
     
-    float d=min(torusDist,planeDist);
-    d=min(d,sphereDist);
-    d=min(d,cubeDist);
+    float d=smin(torusDist,planeDist, 2.0);
+    d=smin(d,sphereDist, 2.0);
+    d=smin(d,cubeDist, 2.0);
     return d;
 }
 
@@ -65,62 +70,62 @@ vec3 estimateNormal(vec3 p){
             sceneSDF(vec3(p.x+EPSILON,p.y,p.z))-sceneSDF(vec3(p.x-EPSILON,p.y,p.z)),
             sceneSDF(vec3(p.x,p.y+EPSILON,p.z))-sceneSDF(vec3(p.x,p.y-EPSILON,p.z)),
             sceneSDF(vec3(p.x,p.y,p.z+EPSILON))-sceneSDF(vec3(p.x,p.y,p.z-EPSILON))
-        ));
+    ));
+}
+    
+vec3 phongContribForLight(vec3 kd,vec3 ks,float alpha,vec3 p,vec3 eye,vec3 lightPos,vec3 lightIntensity){
+    vec3 N=estimateNormal(p);
+    vec3 L=normalize(lightPos-p);
+    vec3 V=normalize(eye-p);
+    vec3 R=normalize(reflect(-L,N));
+    
+    float dotLN=dot(L,N);
+    float dotRV=dot(R,V);
+    
+    float d=rayMarch(p+N*SURF_DIST*2.,L);
+    if(d<length(lightPos-p)){
+        lightIntensity*=.2;
     }
     
-    vec3 phongContribForLight(vec3 kd,vec3 ks,float alpha,vec3 p,vec3 eye,vec3 lightPos,vec3 lightIntensity){
-        vec3 N=estimateNormal(p);
-        vec3 L=normalize(lightPos-p);
-        vec3 V=normalize(eye-p);
-        vec3 R=normalize(reflect(-L,N));
-        
-        float dotLN=dot(L,N);
-        float dotRV=dot(R,V);
-        
-        float d=rayMarch(p+N*SURF_DIST*2.,L);
-        if(d<length(lightPos-p)){
-            lightIntensity*=.1;
-        }
-        
-        if(dotLN<0.){
-            return vec3(0.,0.,0.);
-        }
-        
-        if(dotRV<0.){
-            return lightIntensity*(kd*dotLN);
-        }
-        return lightIntensity*(kd*dotLN+ks*pow(dotRV,alpha));
+    if(dotLN<0.){
+        return vec3(0.,0.,0.);
     }
     
-    vec3 phongIllumination(vec3 ka,vec3 kd,vec3 ks,float alpha,vec3 p,vec3 eye){
-        const vec3 ambientLight=.5*vec3(1.,1.,1.);
-        vec3 color=ambientLight*ka;
-        
-        vec3 lightPos=vec3(4.,10.,4.);
-        vec3 lightIntensity=vec3(.6,.6,.6);
-        
-        color+=phongContribForLight(kd,ks,alpha,p,eye,lightPos,lightIntensity);
-        return color;
+    if(dotRV<0.){
+        return lightIntensity*(kd*dotLN);
     }
+    return lightIntensity*(kd*dotLN+ks*pow(dotRV,alpha));
+}
     
-    void main(){
-        vec2 uv=(gl_FragCoord.xy-.5*resolution.xy)/resolution.y;
-        
-        vec3 col=vec3(0);
-        
-        vec3 ro=vec3(cameraPosition);
-        vec3 rd=normalize(vec3(uv.x,uv.y,1));
-        
-        float d=rayMarch(ro,rd);
-        
-        vec3 p=ro+rd*d;
-        
-        vec3 Ka=vec3(.2,.2,.2);
-        vec3 Kd=vec3(.298,.698,.2);
-        vec3 Ks=vec3(1.,1.,1.);
-        float shininess=10.;
-        
-        col=phongIllumination(Ka,Kd,Ks,shininess,p,ro);
-        
-        gl_FragColor=vec4(col,1.);
-    }
+vec3 phongIllumination(vec3 ka,vec3 kd,vec3 ks,float alpha,vec3 p,vec3 eye){
+    const vec3 ambientLight=.5*vec3(1.,1.,1.);
+    vec3 color=ambientLight*ka;
+    
+    vec3 lightPos=vec3(4.,10.,4.);
+    vec3 lightIntensity=vec3(.6,.6,.6);
+    
+    color+=phongContribForLight(kd,ks,alpha,p,eye,lightPos,lightIntensity);
+    return color;
+}
+    
+void main(){
+    vec2 uv=(gl_FragCoord.xy-.5*resolution.xy)/resolution.y;
+    
+    vec3 col=vec3(0);
+    
+    vec3 ro=vec3(cameraPosition);
+    vec3 rd=normalize(vec3(uv.x,uv.y,1));
+    
+    float d=rayMarch(ro,rd);
+    
+    vec3 p=ro+rd*d;
+    
+    vec3 Ka=vec3(.2,.2,.2);
+    vec3 Kd= estimateNormal(p) / cameraPosition;
+    vec3 Ks=vec3(.2,.2,.2);
+    float shininess=10.;
+    
+    col=phongIllumination(Ka,Kd,Ks,shininess,p,ro);
+    
+    gl_FragColor=vec4(col,1.);
+}
